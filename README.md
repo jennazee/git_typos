3bfb8997c58e71b48c5e2030467a0d75308218fc
Jenna Zeigen <jenna.zeigen@gmail.com>
2014-12-15 10:27:58 -0500

    initial commit of everything

---
+++ README.md
@@ -0,0 +1,8 @@
+git_typos
+=========
+
+A silly twitter bot that just can't seem to type its git commands properly.
+
+(Tweets git + a word that has a Levenshtein edit distance of 1 from a git command)
+
+
---
+++ bot.js
@@ -0,0 +1,48 @@
+var fs = require('fs');
+var Levenshtein = require('levenshtein');
+var Twit = require('twit');
+
+function getGitTypos() {
+  var typos = [];
+  cmds = fs.readFileSync('./cmds.txt', 'utf8').split(/\s+/);
+  fs.readFile('./words.txt', 'utf8', function(err, data) {
+    if (err) throw err
+    words = data.split(/\s+/);
+    for (var i = 0; i < words.length; i++) {
+      for (var j = 0; j < cmds.length; j++) {
+        if (words[i].length > 2) {
+          var l = new Levenshtein(words[i].toLowerCase(), cmds[j]);
+          if (l.distance === 1) {
+            typos.push(words[i].toLowerCase());
+            break
+          }
+        }
+      }
+    }
+    bot(typos);
+  })
+}
+
+function bot(typos) {
+  var T = new Twit({
+      consumer_key:         'REDACTED'
+    , consumer_secret:      'REDACTED'
+    , access_token:         'REDACTED'
+    , access_token_secret:  'REDACTED'
+  })
+
+  postStatus();
+  setTimeout(postStatus, 6*60*60*1000);
+
+  function postStatus() {
+    T.post('statuses/update', { status: 'git ' + typos.shift()}, function(err, data, resp) {
+      if (err) {
+        console.log(err);
+      }
+    })
+  }
+}
+
+getGitTypos();
+
+
